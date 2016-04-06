eb17272472a7ce50fb760a16bcad471d4aaaa5af
Wei-Cheng Yang <rainyday309@gmail.com>
2016-04-06 17:01:36 +0800

    ADD: Login Popup component using Popup as parent component

---
+++ src/react-component/Navbar/LoginPopup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Popup from './Popup';
+
+class LoginPopup extends React.Component {
+	render (){
+		return (
+				<Popup {...this.props}>
+					<img src="/img/kitty.png"/>
+					<h1>Login to Join Community</h1>
+					<p>ProductHunt is a community to share and geek out about the latest code, podcast and new stuff.</p>
+					<button className="facebook-btn">Login with Facebook</button>
+					<p>We'll never post to Facebook without your permission</p>
+				</Popup>
+			);
+	}
+}
+
+export default LoginPopup;
