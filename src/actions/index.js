3955219bc500266c3109ed50ed1127037732c9e4
Wei-Cheng Yang <rainyday309@gmail.com>
2016-04-12 18:54:09 +0800

    ADD: add stores, decorators and connection to refactor data flow with flux structure, add transform-decorators-legacy in package.json for using decorators

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,17 @@ import alt from '../alt';
 import Firebase from 'firebase';
 
 class Actions {
-
+	login (){
+		return (dispatch) => {
+			var firebaseRef = new Firebase('https://producthunt-rainy.firebaseio.com');
+			firebaseRef.authWithOAuthPopup('facebook', (error, user) => {
+				if (error) {
+					return;
+				} 
+				dispatch(user);
+			});	
+		}
+	}
 }
 
 export default alt.createActions(Actions);
